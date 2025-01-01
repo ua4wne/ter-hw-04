c39a86bde002d91cb3572a4616dffe042ae5b942
Alexander Rogatnev <rai68@yandex.ru>
2025-01-01 11:25:25 +0000

    task2 edit

--- src/main.tf
+++ src/main.tf
@@ -6,19 +6,12 @@ module  "vpc_dev_a" {
   cidr = "10.0.1.0/24"
 }
 
-module "vpc_dev_b" {
-  source       = "./vpc"
-  env_name     = "develop"
-  zone = "ru-central1-b"
-  cidr = "10.0.2.0/24"
-}
-
 module "test-vm" {
   source         = "git::https://github.com/udjin10/yandex_compute_instance.git?ref=main"
   env_name       = "develop"
   network_id     = module.vpc_dev_a.network_id
-  subnet_zones   = ["ru-central1-a", "ru-central1-b"]
-  subnet_ids     = [module.vpc_dev_a.id, module.vpc_dev_b.id]
+  subnet_zones   = ["ru-central1-a"]
+  subnet_ids     = [module.vpc_dev_a.id]
   instance_name  = "webs"
   instance_count = 1
   image_family   = "ubuntu-2004-lts"
